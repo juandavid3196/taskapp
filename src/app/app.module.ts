4b583d85b816b4006f770b264b5238f9972767fd
juan david rios <juandavid3196@gmail.com>
2025-01-08 16:46:00 -0500

    Basic project installed,  Basic components and folders, header struture and styling.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TaskFormComponent } from './components/task-form/task-form.component';
+import { TaskListComponent } from './components/task-list/task-list.component';
+import { TaskBoardComponent } from './components/task-board/task-board.component';
+import { StateManagerComponent } from './components/state-manager/state-manager.component';
+import { HeaderComponent } from './components/header/header.component';
+import { DragAndDropDirective } from './directives/drag-and-drop.directive';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TaskFormComponent,
+    TaskListComponent,
+    TaskBoardComponent,
+    StateManagerComponent,
+    HeaderComponent,
+    DragAndDropDirective,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule, MatIconModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/components/state-manager/state-manager.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-state-manager',
+  templateUrl: './state-manager.component.html',
+  styleUrls: ['./state-manager.component.scss']
+})
+export class StateManagerComponent {
+
+}
