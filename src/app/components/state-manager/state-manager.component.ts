ac4596e80002d47cb96d1fde929db85978a64050
juan david rios <juandavid3196@gmail.com>
2025-01-09 11:16:17 -0500

    state-manager CRUD services working

--- src/app/components/state-manager/state-manager.component.ts
+++ src/app/components/state-manager/state-manager.component.ts
@@ -9,6 +9,7 @@ import {
 } from '@angular/forms';
 import { State } from 'src/app/models/state.model';
 import { StateService } from 'src/app/services/state.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-state-manager',
@@ -21,6 +22,8 @@ export class StateManagerComponent {
   @Output() formClose = new EventEmitter<void>();
   stateForm: FormGroup;
   states: State[] = [];
+  update: boolean = false;
+  stateId: string = '';
 
   constructor(private fb: FormBuilder, private stateService: StateService) {
     this.stateForm = this.fb.group({
@@ -49,6 +52,41 @@ export class StateManagerComponent {
     this.onClose();
   }
 
+  onUpdateState(state: State): void {
+    this.stateForm.patchValue({
+      title: state.title,
+    });
+
+    this.update = !this.update;
+    this.stateId = state.id;
+  }
+
+  onEliminateState(state: State): void {
+    Swal.fire({
+      title: '¿Estás seguro?',
+      html: `El estado ${state.title} se eliminará de manera permanente.`,
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Sí, Eliminar',
+    }).then((result) => {
+      if (result.isConfirmed) {
+        this.stateService.deleteState(state.id);
+        Swal.fire('Eliminado', 'El estado ha sido eliminado.', 'success');
+      }
+    });
+  }
+
+  cleanVariables(): void {
+    this.update = false;
+    this.stateForm.patchValue({
+      title: '',
+    });
+    this.stateId = '';
+    this.errorMessage = false;
+  }
+
   onClose(): void {
     this.close = true;
     setTimeout(() => {
@@ -59,10 +97,17 @@ export class StateManagerComponent {
   onSubmit(): void {
     if (this.stateForm.valid) {
       try {
-        console.log(this.stateForm.value);
-        this.stateService.addState(this.stateForm.get('title')?.value);
+        if (this.update) {
+          this.stateService.updateState(
+            this.stateId,
+            this.stateForm.get('title')?.value
+          );
+        } else {
+          this.stateService.addState(this.stateForm.get('title')?.value);
+        }
+        this.cleanVariables();
       } catch (error) {
-        console.error('Error creating task', error);
+        console.error('Error creating state', error);
       }
     } else {
       this.errorMessage = !this.errorMessage;
